2dcf03d07b15b32240db2c2027a482f16f71bcae
Omar EL ATMANI <contact@omaghd.com>
2022-02-03 14:48:08 +0100

    Add Audio Topic Controller with view

---
+++ app/Http/Controllers/AudioTopicController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Audio;
+use App\Models\Topic;
+
+class AudioTopicController extends Controller
+{
+    public function index(Audio $audio)
+    {
+        //
+    }
+
+    public function update(Audio $audio, Topic $topic)
+    {
+        //
+    }
+
+    public function destroy(Audio $audio, Topic $topic)
+    {
+        //
+    }
+}
