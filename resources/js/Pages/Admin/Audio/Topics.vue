2541cf198453243c805f7e77f4926efb1b0ee4f9
Omar EL ATMANI <contact@omaghd.com>
2022-05-09 02:47:01 +0100

    Add FontAwesome & Edit forward seconds

--- resources/js/Pages/Admin/Audio/Topics.vue
+++ resources/js/Pages/Admin/Audio/Topics.vue
@@ -1,5 +1,5 @@
 <template>
-    <Head :title="`&quot;${audio.title}&quot; Topics`"/>
+    <Head :title="`&quot;${audio.title}&quot; Topics`" />
 
     <BreezeAuthenticatedLayout>
         <template #header>
@@ -12,7 +12,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        <form @submit.prevent="submit" class="max-w-md mx-auto mt-8" enctype="multipart/form-data">
+                        <form @submit.prevent="submit" class="max-w-md mx-auto mt-8">
                             <div
                                 v-if="$page.props.flash.message && !processing"
                                 class="bg-indigo-100 border-t-4 border-indigo-500 rounded-b text-indigo-900 px-4 py-3 shadow-md mb-6"
@@ -22,7 +22,7 @@
                                         <svg class="fill-current h-6 w-6 text-indigo-500 mr-4"
                                              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                             <path
-                                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/>
+                                                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
                                         </svg>
                                     </div>
                                     <div>
@@ -38,45 +38,32 @@
                                        :src="`${route('root')}${audio.path.replace('public', '/storage')}`">
                                 </audio>
 
-                                <section class="flex justify-between">
+                                <section class="flex justify-evenly">
                                     <button
                                         type="button"
                                         @click="mark"
                                         class="block items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-700 hover:bg-indigo-600 transition ease-in-out duration-150"
+                                        title="Mark"
                                     >
-                                        Mark
+                                        <font-awesome-icon icon="bookmark" />
                                     </button>
 
                                     <button
                                         type="button"
                                         @click="forward"
                                         class="block items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-700 hover:bg-gray-600 transition ease-in-out duration-150"
+                                        title="Forward"
                                     >
-                                        Forward
+                                        <font-awesome-icon icon="forward" />
                                     </button>
 
                                     <button
                                         type="button"
                                         @click="backward"
                                         class="block items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-700 hover:bg-gray-600 transition ease-in-out duration-150"
+                                        title="Backward"
                                     >
-                                        Backward
-                                    </button>
-
-                                    <button
-                                        type="button"
-                                        @click="play"
-                                        class="block items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-700 hover:bg-gray-600 transition ease-in-out duration-150"
-                                    >
-                                        Play
-                                    </button>
-
-                                    <button
-                                        type="button"
-                                        @click="pause"
-                                        class="block items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-gray-700 hover:bg-gray-600 transition ease-in-out duration-150"
-                                    >
-                                        Pause
+                                        <font-awesome-icon icon="backward" />
                                     </button>
                                 </section>
                             </div>
@@ -91,7 +78,7 @@
                                 <input v-model="form.title"
                                        class="border border-gray-400 rounded p-2 w-full"
                                        type="text"
-                                       name="title" id="title"/>
+                                       name="title" id="title" />
                                 <div v-if="errors.title" v-text="errors.title"
                                      class="text-red-500 text-xs mt-1"></div>
                             </div>
@@ -141,69 +128,69 @@
                                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                                         <table class="table-auto min-w-full divide-y divide-gray-200">
                                             <thead class="bg-gray-50">
-                                            <tr>
-                                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                    Title
-                                                </th>
-                                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                    Time
-                                                </th>
-                                                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                    User
-                                                </th>
-                                                <th scope="col" class="relative px-6 py-3">
-                                                    <span class="sr-only">Options</span>
-                                                </th>
-                                            </tr>
+                                                <tr>
+                                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                        Title
+                                                    </th>
+                                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                        Time
+                                                    </th>
+                                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                        User
+                                                    </th>
+                                                    <th scope="col" class="relative px-6 py-3">
+                                                        <span class="sr-only">Options</span>
+                                                    </th>
+                                                </tr>
                                             </thead>
 
                                             <tbody class="bg-white divide-y divide-gray-200">
-                                            <tr v-for="topic in audio.topics" :key="topic.id">
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="flex items-center">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            {{ topic.title }}
+                                                <tr v-for="topic in audio.topics" :key="topic.id">
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                        <div class="flex items-center">
+                                                            <div class="text-sm font-medium text-gray-900">
+                                                                {{ topic.title }}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </td>
+                                                    </td>
 
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="flex items-center">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            <a
-                                                                href="#audio"
-                                                                title="GO TO"
-                                                                @click="goTo(topic.time)"
-                                                                class="text-indigo-600 hover:text-indigo-900 mr-6">
-                                                                {{ formatSeconds(topic.time) }}
-                                                            </a>
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                        <div class="flex items-center">
+                                                            <div class="text-sm font-medium text-gray-900">
+                                                                <a
+                                                                    href="#audio"
+                                                                    title="GO TO"
+                                                                    @click="goTo(topic.time)"
+                                                                    class="text-indigo-600 hover:text-indigo-900 mr-6">
+                                                                    {{ formatSeconds(topic.time) }}
+                                                                </a>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </td>
+                                                    </td>
 
-                                                <td class="px-6 py-4 whitespace-nowrap">
-                                                    <div class="flex items-center">
-                                                        <div class="text-sm font-medium text-gray-900">
-                                                            <Link :href="this.route('users.topics', topic.user.id)"
-                                                                  title="See all topics"
-                                                                  class="text-indigo-600 hover:text-indigo-900">
-                                                                {{ topic.user.name }}
-                                                            </Link>
+                                                    <td class="px-6 py-4 whitespace-nowrap">
+                                                        <div class="flex items-center">
+                                                            <div class="text-sm font-medium text-gray-900">
+                                                                <Link :href="this.route('users.topics', topic.user.id)"
+                                                                      title="See all topics"
+                                                                      class="text-indigo-600 hover:text-indigo-900">
+                                                                    {{ topic.user.name }}
+                                                                </Link>
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </td>
+                                                    </td>
 
-                                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                                    <Link
-                                                        v-if="topic.user_id === $page.props.auth.user.id || $page.props.auth.user.is_admin"
-                                                        :href="route('topics.destroy', topic.id)"
-                                                        preserve-scroll
-                                                        method="delete"
-                                                        class="text-indigo-600 hover:text-indigo-900">
-                                                        Delete
-                                                    </Link>
-                                                </td>
-                                            </tr>
+                                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                        <Link
+                                                            v-if="topic.user_id === $page.props.auth.user.id || $page.props.auth.user.is_admin"
+                                                            :href="route('topics.destroy', topic.id)"
+                                                            preserve-scroll
+                                                            method="delete"
+                                                            class="text-indigo-600 hover:text-indigo-900">
+                                                            Delete
+                                                        </Link>
+                                                    </td>
+                                                </tr>
                                             </tbody>
                                         </table>
                                     </div>
@@ -225,8 +212,8 @@ const processing = ref(false);
 let audioElement;
 
 let props = defineProps({
-    audio:      Object,
-    errors:     Object,
+    audio: Object,
+    errors: Object,
     processing: Boolean
 });
 
@@ -236,19 +223,19 @@ onMounted(() => {
 
 let form = useForm({
     title: null,
-    time:  null,
+    time: null,
 });
 
 let submit = () => {
     form.post(route('audios.topics', props.audio.id), {
-        onStart:        () => {
+        onStart: () => {
             processing.value = true;
         },
         preserveScroll: true,
-        onSuccess:      () => {
+        onSuccess: () => {
             form.reset();
         },
-        onFinish:       () => {
+        onFinish: () => {
             processing.value = false;
         }
     })
@@ -264,7 +251,7 @@ let backward = () => {
 }
 
 let forward = () => {
-    audioElement.currentTime += 1;
+    audioElement.currentTime += 10;
 }
 
 let pause = () => {
@@ -276,11 +263,7 @@ let play = () => {
 }
 
 let goTo = (seconds) => {
-    console.log('CLICKED');
-    console.log(seconds);
-    console.log(audioElement.currentTime);
     audioElement.currentTime = seconds;
-    console.log(audioElement.currentTime);
 }
 
 let formatSeconds = (seconds) => {
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,19 @@ import { createApp, h } from 'vue';
 import { createInertiaApp, Head, Link } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { dom } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fas);
+dom.watch();
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'OAudio';
 
 createInertiaApp({
-    title:   (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}.vue`),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
@@ -17,6 +25,7 @@ createInertiaApp({
             .component('Head', Head)
             .component('Link', Link)
             .component('BreezeAuthenticatedLayout', BreezeAuthenticatedLayout)
+            .component("font-awesome-icon", FontAwesomeIcon)
             .mount(el);
     },
 });
