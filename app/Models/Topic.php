84826d8a260ee6143f4948da4d35d776995039df
Omar EL ATMANI <contact@omaghd.com>
2022-05-09 05:33:48 +0100

    Enable soft delete for User & Topic models

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Topic extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['title', 'time', 'is_done', 'done_at', 'user_id', 'audio_id'];
 
